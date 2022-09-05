cf92cb0e899973b1f8ce5ce2ef8246c49df13974
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-09-05 23:34:59 +0200

    part 1.1 done

---
+++ part1/src/App.js
@@ -0,0 +1,38 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.partTitle} {props.subtotal}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercices {props.total}</p>
+}
+
+const App = () => {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content partTitle={part1} subtotal={exercises1} />
+      <Content partTitle={part2} subtotal={exercises2} />
+      <Content partTitle={part3} subtotal={exercises3} />
+      <Total total={total} />
+    </div>
+  )
+}
+
+export default App
