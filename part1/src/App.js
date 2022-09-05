3456d183651f214f11f73d27fa7178d5655d0290
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-09-06 00:20:10 +0200

    part 1.2 done

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,35 +2,56 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
-      {props.partTitle} {props.subtotal}
+      {props.part.title} {props.part.subtotal}
     </p>
   )
 }
 
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+    // Here we could obviously iterate over props.parts with
+    // <div>{props.parts.map(part => <Part part={part} />)}</div>
+  )
+}
+
 const Total = (props) => {
-  return <p>Number of exercices {props.total}</p>
+  return (
+    <p>
+      <strong>Number of exercices {props.total}</strong>
+    </p>
+  )
 }
 
 const App = () => {
   const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3
+  const parts = [
+    { title: "Fundamentals of React", subtotal: 10 },
+    { title: "Using props to pass data", subtotal: 7 },
+    { title: "State of a component", subtotal: 14 },
+  ]
+
+  // Computing total in a simple way
+  let total = 0
+  parts.forEach((part) => (total += part.subtotal))
+
+  // Same in a prettier way
+  const total2 = parts
+    .map(part => part.subtotal)
+    .reduce((previousVal, currentVal) => previousVal + currentVal, 0)
 
   return (
     <div>
       <Header course={course} />
-      <Content partTitle={part1} subtotal={exercises1} />
-      <Content partTitle={part2} subtotal={exercises2} />
-      <Content partTitle={part3} subtotal={exercises3} />
-      <Total total={total} />
+      <Content parts={parts} />
+      <Total total={total2} />
     </div>
   )
 }
