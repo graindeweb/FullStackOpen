e47e738d5d36a43ea5f7b83fb44579950f476a3f
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-09-07 01:38:45 +0200

    Part 1.8 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -23,17 +23,37 @@ const Result = ({ text, count }) => {
   )
 }
 
+const Statistics = ({feedbacks}) => {
+  const total = Object.values(feedbacks).reduce((p, v) => p + v, 0)
+
+  const computeAverage = () => {
+    return total ? (feedbacks.good * 1 + feedbacks.bad * -1) / total : 0
+  }
+
+  const computePositive = () => {
+    return total ? (feedbacks.good * 100) / total : 0
+  }
+  return (
+    <section>
+      <Result text={statTypes["good"]} count={feedbacks.good} />
+      <Result text={statTypes["neutral"]} count={feedbacks.neutral} />
+      <Result text={statTypes["bad"]} count={feedbacks.bad} />
+      <Result text="All" count={total} />
+      <Result text="Average" count={computeAverage()} />
+      <Result text="Positive" count={`${computePositive()} %`} />
+    </section>
+  )
+}
+
 function App() {
   // Save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
 
   // Same increment for all buttons
   const incrementStat = (setter, value) => {
     setter(value + 1)
-    setTotal(total + 1)
   }
 
   // Just playing with function that returns function
@@ -49,13 +69,6 @@ function App() {
     }
   }
 
-  const computeAverage = () => {
-    return total ? (good * 1 + bad * -1) / total : 0
-  }
-
-  const computePositive = () => {
-    return total ? good * 100 / total : 0
-  }
 
   return (
     <div>
@@ -65,12 +78,7 @@ function App() {
       ))}
 
       <Header title="statistics" />
-      <Result text={statTypes["good"]} count={good} />
-      <Result text={statTypes["neutral"]} count={neutral} />
-      <Result text={statTypes["bad"]} count={bad} />
-      <Result text="All" count={total} />
-      <Result text="Average" count={computeAverage()} />
-      <Result text="Positive" count={`${computePositive()} %`} />
+      <Statistics feedbacks={{good: good, bad: bad, neutral: neutral}} />
     </div>
   )
 }
